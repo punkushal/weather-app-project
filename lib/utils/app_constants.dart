515d9d7973f9f92e77e8e9e93fafa65d870d9033
punkushal <spycymagar@gmail.com>
2025-01-08 16:54:57 +0545

    app constants added

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String apiKey = '2f26dcfd18e342bab2162421240709';
+  static const String baseUrl = 'http://api.weatherapi.com/v1';
+  static const String weatherEndpoint = '/current.json';
+
+  // SharedPreferences keys
+  static const String locationKey = 'saved_location';
+  static const String firstLaunchKey = 'first_launch';
+}
